436ba4cb1d6fc2bbdd445ac3dac307b1c77dd4d6
Christopher Reed <84865108+mr-chris-reed@users.noreply.github.com>
2022-11-07 01:09:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+class Main {
+  public static void main(String[] args) {
+    /*
+    * Problem 1
+    * Use nested for loops to make a rectangle
+    * with "*"-s.  Make it 10 stars by
+    * 10 lines.  Your solution must use nested
+    * for loops.
+    */
+
+    /*
+    * Problem 2
+    * Use a for loop or while loop to reverse a
+    * string.  You must use the substring method
+    * in your reverse process.  You may not convert
+    * the string to a character array.
+    */
+
+    /*
+    * Problem 3
+    * Use either a while or for loop to
+    * generate the first n Fibonacci numbers.
+    * Print each Fibonacci number.
+    * For information about Fibonacci numbers,
+    * please see https://en.wikipedia.org/wiki/Fibonacci_number
+    */
+
+  }
+}
